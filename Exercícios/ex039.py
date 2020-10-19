9738bee356da3f3d02e6c5f0462cc8da40c5123b
Renato Bastos Pope <renato@pope.med.br>
2020-10-19 14:49:14 -0300

    Update ex039.py

--- Exercícios/ex039.py
+++ Exercícios/ex039.py
@@ -1,7 +1,7 @@
 from datetime import date
 atual = date.today().year
 ano_nasc = int(input('Qual o ano do seu nascimento: '))
-idade = ano_nasc - atual
+idade = atual-ano_nasc
 print('Quem nasceu em {} tem {} anos em {}'.format(ano_nasc, idade, atual))
 if idade == 18:
     print('Você tem que se alistar IMEDIATAMENTE.')
@@ -11,7 +11,7 @@ elif idade < 18:
     print('Ainda faltam {} anos para o seu alistamento'.format(saldo))
     print('O seu alistamento será em {}.'.format(alistamento))
 elif idade > 18:
-    saldo = idade - 18
-    alistamento = atual - saldo
+    saldo = int(idade - 18)
+    alistamento = int(atual - saldo)
     print('Você deveria ter se alistados há {} anos.'.format(saldo))
     print('Seu alistamento foi em {}.'.format(alistamento))
