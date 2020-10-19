dffcb814bb608961d7284e770a8b90aedaee3f1e
Renato Bastos Pope <renato@pope.med.br>
2020-10-19 15:08:18 -0300

    corrigido

---
+++ Exercícios/ex042.py
@@ -0,0 +1,21 @@
+cores = {'inv': '\033[7m',
+         'limpa': '\033[m',
+         'verm': '\033[1;31m'}
+print('===' * 8)
+print('{}ANALISADOR DE TRIANGULO{}'.format(cores['inv'], cores['limpa']))
+print('===' * 8)
+x = float(input('Digite o primeiro lado do triangulo: '))
+y = float(input('Digite o segundo lado do triangulo: '))
+z = float(input('Digite o terceiro lado do triangulo: '))
+nt = x >= y + z or y >= z + x or z >= x + y
+if nt:
+    print('Essas medidas {}não{} formam um triangulo!'.format(cores['verm'], cores['limpa']))
+else:
+    print('As medidas dadas formam um triangulo.')
+if not nt:
+    if x == y and x == z:
+        print('É um triangulo equilatero.')
+    elif x != z and x != y and z != y:
+        print('É um triangulo escaleno.')
+    elif x == y or x == z or y == z:
+        print('É um triangulo isosceles.')
---
+++ Exercícios/ex043.py
@@ -0,0 +1,14 @@
+p = float(input('Qual o peso em Kg: '))
+a = float(input('Qual a altura em m: '))
+imc = p /(a)**2
+print('O IMC do indivíduo é {:.2f}Kg/m2 e'.format(imc), end=' ')
+if imc <= 18.5:
+    print('o indivíduo está abaixo do peso.')
+elif 18.5 < imc <= 25:
+    print('o indivíduo está na faixa de peso ideal.')
+elif 25 < imc <= 30:
+    print('o indivíduo está com sobrepeso')
+elif 30 < imc <= 40:
+    print('o indivíduo está obeso.')
+elif imc > 40:
+    print('o indivíduo está com obesidade mórbida.')
