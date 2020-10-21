69dfd908c660a60396aeb73a6393bf47e8c42805
Renato Bastos Pope <renato@pope.med.br>
2020-10-20 22:32:25 -0300

    excicios

---
+++ Exercícios/ex052.py
@@ -0,0 +1,13 @@
+num = int(input('Digite um número: '))
+tot = 0
+for c in range(1, num+1):
+    if num % c == 0:
+        print('\033[7;31m{}\033[m '.format(c, end=''))
+        tot += 1
+    else:
+        print('{} '.format(c, end=''))
+print('O número {} foi divisível {} vezes.'.format(num, tot))
+if tot == 2:
+    print('E por isso ele é um número primo.')
+else:
+    print('E por isso ele não é um número primo.')
---
+++ Exercícios/ex053.py
@@ -0,0 +1,4 @@
+frase = str(input('Digite uma frase: ')).strip().upper()
+f = frase.upper()
+s = f.strip()
+print(frase)
