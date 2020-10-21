aca0dd9b998987890b34faaa7178c222645c3fe9
Renato Bastos Pope <renato@pope.med.br>
2020-10-21 08:35:05 -0300

    exercicio novo

--- Exercícios/ex052.py
+++ Exercícios/ex052.py
@@ -2,10 +2,10 @@ num = int(input('Digite um número: '))
 tot = 0
 for c in range(1, num+1):
     if num % c == 0:
-        print('\033[7;31m{}\033[m '.format(c, end=''))
+        print('\033[7;31m{}\033[m '.format(c, end=' '))
         tot += 1
     else:
-        print('{} '.format(c, end=''))
+        print('{} '.format(c, end=' '))
 print('O número {} foi divisível {} vezes.'.format(num, tot))
 if tot == 2:
     print('E por isso ele é um número primo.')
---
+++ Exercícios/ex056.py
@@ -0,0 +1,24 @@
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomehomemvelho = ''
+totmulher20 = 0
+for p in range(1, 6):
+    print('------{}ª pessoa------'.format(p))
+    nome = str(input('Qual o primeiro nome da pessoa? ')).strip().capitalize()
+    idade = int(input('Qual a idade da pessoa? '))
+    gen = str(input('Qual o genero [M/F]: ')).strip()
+    somaidade += idade
+    if p == 1 and gen in 'Mm':
+        nomehomemvelho = nome
+        maioridadehomem = idade
+    if gen in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomehomemvelho = nome
+    if gen in 'Ff' and idade < 20:
+        totmulher20 += 1
+
+mediaidade = somaidade / 4
+print('A média de idade do grupo é {}.'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomehomemvelho))
+print('Temos um total de {} mulheres com menos de 20 anos'.format(totmulher20))
