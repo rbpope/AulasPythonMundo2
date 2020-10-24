0402e39e448a34378680beb452fac6e4cf2da66c
Renato Bastos Pope <renato@pope.med.br>
2020-10-24 20:41:44 -0300

    Update ex071.py

--- Exercícios/ex071.py
+++ Exercícios/ex071.py
@@ -3,14 +3,25 @@ print('{:^30} Banco Pope')
 print('='*30)
 valor = int(input('Digite o valor desejado R$'))
 total = valor
-ced = 50
+ced: int = 50
 totced = 0
 while True:
     if total >= ced:
         total -= ced
-        totalced += 1
+        totced += 1
     else:
         if totced > 0:
-            print(f'Total de {totced} de cédulas de R${ced}.')
+            print(f'Total de {totced} cédulas de R${ced:.2f}.')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
 
 
+print('='*30)
+print('Volte Sempre')
