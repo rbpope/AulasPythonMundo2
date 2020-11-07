c7ab8542a662213f1cc5b05a564bf6799fd17804
Renato Bastos Pope <renato@pope.med.br>
2020-11-07 17:41:24 -0300

    ex102 resolvido

--- Exercícios/ex102.py
+++ Exercícios/ex102.py
@@ -1,13 +1,12 @@
 def fatorial(n, show=False):
-
     """
-    O fatorial de um número é calculado pela multiplicação desse número
-    por todos os seus antecessores até chegar ao número 1.
-    Note que nesses produtos, o zero (0) é excluído.
-    O fatorial é representado por: n! = n. (n – 1). (n – 2). (n – 3)!
-    :param n: O número a ser calculado
-    :param show: (opcional) Mostrar ou não a conta
-    :param return: o retorno do fatorial de um número n
+        -> O fatorial de um número é calculado pela multiplicação desse número
+        por todos os seus antecessores até chegar ao número 1.
+        Note que nesses produtos, o zero (0) é excluído.
+        O fatorial é representado por: n! = n. (n – 1). (n – 2). (n – 3)!
+        :param n: O número a ser calculado
+        :param show: (opcional) Mostrar ou não a conta
+        :param return: o retorno do fatorial de um número n
     """
     f = 1
     for c in range(n, 0, -1):
@@ -21,7 +20,6 @@ def fatorial(n, show=False):
     return f
 
 
-#n = int(input('Digite um número: '))
-#print(fatorial(n, True))
+n = int(input('Digite um número: '))
+print(fatorial(n, True))
 
-help(fatorial)
